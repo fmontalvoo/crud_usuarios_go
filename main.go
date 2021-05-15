c291543413cc82a7895d91ef2f8403ef62e67ee9
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-15 18:07:46 -0500

    Leer datos desde el teclado.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"strings"
+)
 
 func crearUsuario(){}
 
@@ -13,13 +18,25 @@ func eliminarUsuario() {}
 func main() {
 
 	var opcion string
+	var reader *bufio.Reader
+
+	reader = bufio.NewReader(os.Stdin)
 
 	fmt.Println("1. Crear")
 	fmt.Println("2. Listar")
 	fmt.Println("3. Actualizar")
 	fmt.Println("4. Eliminar")
-	fmt.Println("Opcion: ")
-	fmt.Scanf("%s\n", &opcion)
+	fmt.Print("Opcion: ")
+
+	// Terminal la lectura hasta que el usuario presiona la tecla Enter.
+	opcion, err := reader.ReadString('\n') 
+
+	if err != nil {
+		panic("Algo salio mal :(")
+	}
+
+	// Elimina los saltos de linea de la opcion.
+	opcion = strings.TrimSpace(opcion)
 
 	switch opcion {
 		case "1", "crear":
@@ -31,6 +48,6 @@ func main() {
 		case "4", "Eliminar":
 			eliminarUsuario()
 		default:
-			fmt.Println("Opcion no valida")
+			fmt.Println("Opcion:", opcion, "no valida")
 	}
 }
