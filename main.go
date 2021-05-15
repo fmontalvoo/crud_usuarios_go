c41eaf679ac0544df8436497b1846f37116fa0a5
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-15 18:15:44 -0500

    Refactorizacion del codigo.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var reader *bufio.Reader
+
 func crearUsuario(){}
 
 func listarUsuario() {}
@@ -15,39 +17,51 @@ func actualizarUsuario() {}
 
 func eliminarUsuario() {}
 
+func leerTeclado()string {
+
+	// Terminal la lectura hasta que el usuario presiona la tecla Enter.
+	if opcion, err := reader.ReadString('\n'); err != nil {
+		panic("Algo salio mal :(")
+	}else{
+		// Elimina los saltos de linea de la opcion.
+		return strings.TrimSpace(opcion)
+	}
+
+}
+
 func main() {
 
 	var opcion string
-	var reader *bufio.Reader
 
 	reader = bufio.NewReader(os.Stdin)
 
-	fmt.Println("1. Crear")
-	fmt.Println("2. Listar")
-	fmt.Println("3. Actualizar")
-	fmt.Println("4. Eliminar")
-	fmt.Print("Opcion: ")
+	for {
+		fmt.Println("1. Crear")
+		fmt.Println("2. Listar")
+		fmt.Println("3. Actualizar")
+		fmt.Println("4. Eliminar")
+		fmt.Print("Opcion: ")
+	
+		opcion = leerTeclado()
 
-	// Terminal la lectura hasta que el usuario presiona la tecla Enter.
-	opcion, err := reader.ReadString('\n') 
-
-	if err != nil {
-		panic("Algo salio mal :(")
+		if opcion == "quit" || opcion == "q"{
+			break
+		}
+	
+		switch opcion {
+			case "1", "crear":
+				crearUsuario()
+			case "2", "listar":
+				listarUsuario()
+			case "3", "actualizar":
+				actualizarUsuario()
+			case "4", "Eliminar":
+				eliminarUsuario()
+			default:
+				fmt.Println("Opcion:", opcion, "no valida")
+		}
 	}
 
-	// Elimina los saltos de linea de la opcion.
-	opcion = strings.TrimSpace(opcion)
+	fmt.Println("Adios...")
 
-	switch opcion {
-		case "1", "crear":
-			crearUsuario()
-		case "2", "listar":
-			listarUsuario()
-		case "3", "actualizar":
-			actualizarUsuario()
-		case "4", "Eliminar":
-			eliminarUsuario()
-		default:
-			fmt.Println("Opcion:", opcion, "no valida")
-	}
 }
